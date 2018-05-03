51b8d771263f8268fc6b6535368ffe6db311e797
Marcel Melo <marcel.msmelo@gmail.com>
2018-05-03 15:20:37 -0300

    Método HTTP Rota carro esportivos

--- routes/CarsRoutes.js
+++ routes/CarsRoutes.js
@@ -1,5 +1,5 @@
 const controller = require('../controllers/CarsController.js');
 module.exports = (app) => {
-    app.put('/carros/esportivos', controller.esportivos);
+    app.get('/carros/esportivos', controller.esportivos);
     app.get('/carros/classicos', controller.classicos);
 }
