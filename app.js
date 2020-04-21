e8f994303569ae539affa8527973362c9cd01c17
Marcel Melo <marcel.msmelo@gmail.com>
2020-04-21 13:51:28 -0300

    API finalizada

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(function(req, res, next) {
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //==========================================================
 //================= Banco de Dados =========================
@@ -38,9 +40,9 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 app.get('*', function(req, res){
-    res.sendFile('./index.html');
+    res.status(200);
+    res.sendFile('index.html');
 });
 
 
@@ -76,10 +78,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        // res.render('error', {
+        //     message: err.message,
+        //     error: err
+        // });
     });
 }
 
@@ -87,10 +89,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    // res.render('error', {
+    //     message: err.message,
+    //     error: {}
+    // });
 });
 
 
--- getAllNews.js
+++ getAllNews.js
@@ -2,11 +2,11 @@ global.logger = require('winston');
 logger.remove(logger.transports.Console)
 logger.add(logger.transports.Console, { colorize: true });
 logger.level = 'debug';
-let id = 14000
+let id = 1000
 
 let loopObj = setInterval(() => {
     logger.info('GET NEWS ID=' + id)
     require('./lib/atualizarNoticias.js')(id)
     id++
-    if (id == 14300) clearInterval(loopObj)
+    if (id == 10000) clearInterval(loopObj)
 }, 5000)
